31b1e2699b24a7bdcd4e7c6bf7bf882f7b9f2683
Jason Zenarosa <jasonzenarosa@gmail.com>
2022-09-07 21:38:39 -0700

    Created buttons and numbutton functions

--- script.js
+++ script.js
@@ -4,18 +4,63 @@ function operate(x, y, operator) {
 };
 
 function add(x, y) {
-    return x + y
+    return x + y;
 };
 
 function subtract(x, y) {
-    return x - y
+    return x - y;
 };
 
 function multiply(x, y) {
-    return x * y
+    return x * y;
 };
 
 function divide(x, y) {
     if (y !== 0) return x / y;
-    else return 'ERROR'
-}
+    else return 'ERROR';
+};
+
+let screen = document.querySelector('.screen');
+let num1;
+let num2;
+let operator;
+
+function numberButtonPress(num) {
+    let cur = screen.innerHTML;
+    cur = Number(cur) * 10 + num;
+    cur = String(cur);
+    screen.textContent = cur;
+};
+
+const numberStrings = [
+    '.zero',
+    '.one',
+    '.two',
+    '.three',
+    '.four',
+    '.five',
+    '.six',
+    '.seven',
+    '.eight',
+    '.nine',
+];
+
+for (let i = 0; i < 10; i++) {
+    let numButton = document.querySelector(numberStrings[i]);
+    numButton.addEventListener('click', _ => numberButtonPress(i));
+};
+
+// const operatorStrings = {
+//     '.add': add,
+//     '.subtract': subtract,
+//     '.divide': divide,
+//     '.multiply': multiply,
+// };
+
+// for (const i in operatorStrings) {
+//     let operatorButton = document.querySelector(i);
+//     operatorButton.addEventListener('click', _ => {
+    
+//     });
+// };
+
